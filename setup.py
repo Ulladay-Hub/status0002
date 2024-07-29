92b4ce36e3aaa7559ec4a8540ce11873b41ca5cd
br <123034682+Ulladay-Hub@users.noreply.github.com>
2024-07-29 19:38:40 +1000

    Update setup.py

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='status0002',
+    version='0.1',
+    packages=find_packages(),
+    description='A custom library for managing virtual environments with different OS types.',
+    author='bradinator',
+    author_email='imnotamilkglass@gmail.com',  # Replace with your actual email address
+    url='https://github.com/Ulladay-Hub/status0002',  # Replace with the actual URL of your project if applicable
+    install_requires=[],
+    python_requires='>=3.6',  # Specify the minimum Python version required
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    project_urls={
+        'Source': 'https://github.com/Ulladay-Hub/status0002',  # Replace with the actual URL of your project if applicable
+    },
+)
